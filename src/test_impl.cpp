52c0160a02c3c80127aa3f7c5b0cad3e1d7e9a1a
michaldanaj <michal.danaj@gmail.com>
2016-11-05 16:54:49 +0100

    Poprawine tworzenie Namespace przez roxygen2

--- src/test_impl.cpp
+++ src/test_impl.cpp
@@ -1,6 +1,10 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+//'@importFrom Rcpp evalCpp
+//'@useDynLib cauchyTest
+
+
 // This is a simple example of exporting a C++ function to R. You can
 // source this function into an R session using the Rcpp::sourceCpp
 // function (or via the Source button on the editor toolbar). Learn
